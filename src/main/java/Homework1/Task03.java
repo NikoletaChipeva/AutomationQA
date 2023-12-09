626500ff7a50ce860900cfa1cb20853c95e2c782
Nikoleta Chipeva <nikoleta.ilkova@gmail.com>
2023-12-09 14:19:24 +0200

    Task03

--- src/main/java/Homework1/Task03.java
+++ src/main/java/Homework1/Task03.java
@@ -1,4 +1,31 @@
 package Homework1;
 
+import java.util.Scanner;
+
 public class Task03 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = Integer.parseInt(scanner.nextLine());
+        int number2 = Integer.parseInt(scanner.nextLine());
+        int number3 = Integer.parseInt(scanner.nextLine());
+
+        //Размяна на стойностите
+        int tempNumber = number1;
+        number1 = number2;
+        number2 = number3;
+        number3 = tempNumber;
+
+        int sum1 = number1 + number2;
+        int sum2 = sum1 - number3;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+
+        System.out.println("Резултат1: " + sum1);
+        System.out.println("Резултат2: " + sum2);
+
+    }
 }
