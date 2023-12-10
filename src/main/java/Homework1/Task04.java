7c9faf0036b4fb5e93ac4d017045949b5e89fd4a
Nikoleta Chipeva <nikoleta.ilkova@gmail.com>
2023-12-10 12:33:28 +0200

    Task04

--- src/main/java/Homework1/Task04.java
+++ src/main/java/Homework1/Task04.java
@@ -1,4 +1,32 @@
 package Homework1;
 
+import java.util.Scanner;
+
 public class Task04 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int hours = Integer.parseInt(scanner.nextLine());
+        double amount = Double.parseDouble(scanner.nextLine());
+        boolean dayOff = Boolean.parseBoolean(scanner.nextLine());;
+
+        //Ако не е почивен ден ще работя
+        if (dayOff) {
+            System.out.println("Ще работя!");
+        }
+        // Ако имам пари и е почивен ден, ще отида на кино
+        if (amount > 0 && dayOff) {
+            System.out.println("Ще отида на кино.");
+        }
+        // Ако нямам пари, ще стоя вкъщи и ще гледам телевизия
+        else {
+            System.out.println("Ще стоя вкъщи и ще гледам телевизия.");
+        }
+        // Ако е почивен ден и имам по-малко от 10 лв, ще отида за сладолед
+        if (dayOff && amount < 10) {
+            System.out.println("Ще отида за сладолед.");
+        }
+
+    }
 }
