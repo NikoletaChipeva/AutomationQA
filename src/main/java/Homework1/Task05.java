ad1c023431727f3e9649274d578cc045cf8da83f
Nikoleta Chipeva <nikoleta.ilkova@gmail.com>
2023-12-10 19:09:12 +0200

    Task04-07

--- src/main/java/Homework1/Task05.java
+++ src/main/java/Homework1/Task05.java
@@ -1,4 +1,16 @@
 package Homework1;
 
 public class Task05 {
+    public static void main(String[] args) {
+
+        //Възходящ ред
+
+        for (int i = 1; i <=100; i++) {
+            System.out.println(i);
+        }
+      // Низходящ ред
+        for (int i = 100; i >=1; i--) {
+            System.out.println(i);
+        }
+    }
 }
--- src/main/java/Homework1/Task06.java
+++ src/main/java/Homework1/Task06.java
@@ -1,4 +1,43 @@
 package Homework1;
 
+import jdk.dynalink.support.SimpleRelinkableCallSite;
+
+import java.util.Scanner;
+
 public class Task06 {
+
+    Scanner scanner = new Scanner(System.in);
+
+    int dayNumber = Integer.parseInt(scanner.nextLine());
+
+    if (dayNumber < 1 || dayNumber > 7 ) {
+        System.out.println("Invalid number");
+    }
+
+    switch (dayNumber) {
+        case 1:
+        System.out.println("Monday");
+        break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+            break;
+        case 6:
+            System.out.println("Saturday");
+            break;
+        case 7:
+            System.out.println("Sunday");
+            break;
+
+    }
+
+
 }
