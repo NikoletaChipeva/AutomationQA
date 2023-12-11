f29fcdc4f5efe47867382167a5e6d9d4b7402aed
Nikoleta Chipeva <nikoleta.ilkova@gmail.com>
2023-12-11 19:49:25 +0200

    Homework1

--- src/main/java/Homework1/Taks01.java
+++ src/main/java/Homework1/Taks01.java
@@ -1,4 +1,25 @@
 package Homework1;
-
+import java.util.Scanner;
 public class Taks01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Въведете своето първо цяло число");
+        int number1 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Въведеното от Вас първо цяло число е " + number1);
+
+        System.out.println("Въведете своето второ цяло число");
+        int number2 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Въведеното от Вас второ цяло число е " + number2);
+
+        System.out.println("Въведете своето трето цяло число");
+        int number3 = Integer.parseInt(scanner.nextLine());
+
+        if (number1 < number3 || number3 > number2) {
+            System.out.println("Въведеното число е средна стойност между Вашето първо и второ цяло число");
+        } else {
+            System.out.println("Въведеното число е извън диапазона на въведените първо и второ цяло число");
+        }
+
+    }
 }
--- src/main/java/Homework1/Task06.java
+++ src/main/java/Homework1/Task06.java
@@ -1,43 +1,43 @@
 package Homework1;
 
-import jdk.dynalink.support.SimpleRelinkableCallSite;
+
 
 import java.util.Scanner;
 
 public class Task06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    Scanner scanner = new Scanner(System.in);
+        int dayNumber = Integer.parseInt(scanner.nextLine());
 
-    int dayNumber = Integer.parseInt(scanner.nextLine());
+        if (dayNumber > 7 || dayNumber <=0) {
 
-    if (dayNumber < 1 || dayNumber > 7 ) {
-        System.out.println("Invalid number");
+            System.out.println("Incorrect input");
+        }
+
+        switch (dayNumber) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+
     }
-
-    switch (dayNumber) {
-        case 1:
-        System.out.println("Monday");
-        break;
-        case 2:
-            System.out.println("Tuesday");
-            break;
-        case 3:
-            System.out.println("Wednesday");
-            break;
-        case 4:
-            System.out.println("Thursday");
-            break;
-        case 5:
-            System.out.println("Friday");
-            break;
-        case 6:
-            System.out.println("Saturday");
-            break;
-        case 7:
-            System.out.println("Sunday");
-            break;
-
-    }
-
-
 }
