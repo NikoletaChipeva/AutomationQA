8a5f9e53ec544a9c84e034710602f26c4d43f4f8
Nikoleta Chipeva <nikoleta.ilkova@gmail.com>
2023-12-15 15:16:07 +0200

    Homework2

--- src/main/java/Homework2/Task01.java
+++ src/main/java/Homework2/Task01.java
@@ -1,4 +1,13 @@
 package Homework2;
 
 public class Task01 {
+
+    public static void sum (int a, int b) {
+        int result = (a + b);
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        sum (40, 50);
+    }
 }
