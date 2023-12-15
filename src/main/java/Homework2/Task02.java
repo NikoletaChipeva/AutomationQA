d9ce48bedb9d9bf5b268a043b22c480ec58911c6
Nikoleta Chipeva <nikoleta.ilkova@gmail.com>
2023-12-15 15:11:59 +0200

    Homework2

---
+++ src/main/java/Homework2/Task02.java
@@ -0,0 +1,14 @@
+package Homework2;
+
+public class Task02 {
+
+    public static void average (int a, int b)
+    {
+        int result = (a + b)/2;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        average (20, 50);
+    }
+}
---
+++ src/main/java/Homework2/Task04.java
@@ -0,0 +1,15 @@
+package Homework2;
+
+//Да се състави метод, който приема като аргументи две числа и отпечатва на екрана техния сбор.
+
+public class Task04 {
+
+    public static void sum (int a, int b) {
+        int sumOfNumbers = a+b;
+        System.out.println(sumOfNumbers);
+    }
+
+    public static void main(String[] args) {
+        sum(10,20);
+    }
+}
