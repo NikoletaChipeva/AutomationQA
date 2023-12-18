ce31a06d08875568b4a7ba71ed87c46847007406
nikoleta.chipeva <nikoleta.ilkova@gmail.com>
2023-12-18 16:45:21 +0200

    Homework2

--- src/main/java/Homework2/Task05.java
+++ src/main/java/Homework2/Task05.java
@@ -5,4 +5,18 @@ package Homework2;
 // След създаването на масива да се копират стойностите в нов масив
 
 public class Task05 {
+    public static void main(String[] args) {
+
+        int [] numbers = {10, 12, 1, 8, 4};
+
+        int [] newNumbers = new int [numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            newNumbers [i] = numbers[i];
+        }
+        for (int number : newNumbers) {
+            System.out.println(number);
+        }
+
+    }
 }
